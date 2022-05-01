91bc3b4e1f5d00f9e6392813f0fcb67d94e5c006
Henriqueflino <lino.henrique.henrique@gmail.com>
2022-05-01 16:01:48 -0300

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
+  Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == 'none') document.getElementById(el).style.display = 'block';
+  }
+
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http
+      .get<any>('http://localhost:3000/?busca=CMRG1')
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
-
